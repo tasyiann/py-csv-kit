a72f77a1eb4596083d5374ca5164170a677ff085
Anastasios <tasyiann@gmail.com>
2021-10-29 23:44:34 +0300

    updates

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ if __name__ == '__main__':
     # Initiate Data Processor
     data_processor = DataProcessing("./data/pricat.csv", "./data/mappings.csv")
 
+    # todo: Report any abnormalities & Discard any faulty mappings
+    data_processor.sanitize_data()
+
     # Format price catalogue using the mappings.
-    updated_pricat = data_processor.format_pricat_with_mappings()
+    updated_header, updated_pricat = data_processor.format_pricat_with_mappings()
 
     # Export updated price catalogue in csv format.
-    data_processor.export_in_csv(updated_pricat, "./results/mapped_pricat.csv")
+    data_processor.export_in_csv(updated_header, updated_pricat, "./results/mapped_pricat.csv")
 
-    # Grouping process
-    pass
+    # todo: Execute the Grouping process.
+    # structured_dict = data_processor.create_structured_dictionary(updated_pricat)
+
+    # todo: Export results.
+    # data_processor.export_as_JSON(structured_dict, "./results/structured_data.json")
--- utils.py
+++ utils.py
@@ -4,8 +4,12 @@ import csv
 class DataProcessing:
 
     def __init__(self, pricat_filename="./data/pricat.csv", mappings_filename="./data/mappings.csv"):
-        self.fieldnames, self.pricat = self._read_csv(pricat_filename)
+        self.pricat_header, self.pricat = self._read_csv(pricat_filename)
         _, self.mappings = self._read_csv(mappings_filename)
+
+    def sanitize_data(self):
+        # todo: discard any faulty mappings & report them
+        pass
 
     @staticmethod
     def _read_csv(file_name):
@@ -20,15 +24,19 @@ class DataProcessing:
                 return False
         return True
 
-    def _update_fieldnames(self, source_type, destination_type):
-        if source_type in self.fieldnames:
-            self.fieldnames.remove(source_type)
-        if destination_type not in self.fieldnames:
-            self.fieldnames.append(destination_type)
+    @staticmethod
+    def _update_header(header, source_type, destination_type):
+        if source_type in header:
+            header.remove(source_type)
+        if destination_type not in header:
+            header.append(destination_type)
 
     def format_pricat_with_mappings(self):
-
+        # todo: try other methods instead of brute force.
+        #       do plots and explain the improvements
+        #       Maybe see it as a 2D matrix? + 1D of fields?
         pricat_new = self.pricat.copy()
+        header_new = self.pricat_header.copy()
 
         for m in self.mappings:
 
@@ -44,14 +52,14 @@ class DataProcessing:
                 for source_type in source_types:
                     if source_type in shoe_config_new and source_type != m['destination_type']:
                         shoe_config_new.pop(source_type)
-                        self._update_fieldnames(source_type, m['destination_type'])
+                        self._update_header(header_new, source_type, m['destination_type'])
 
-        return pricat_new
+        return header_new, pricat_new
 
-    def export_in_csv(self, pricat, filename):
+    def export_in_csv(self, header, pricat, filename):
         # todo: remove empty columns?
         with open(filename, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames, delimiter=";")
+            writer = csv.DictWriter(csvfile, fieldnames=header, delimiter=";")
             writer.writeheader()
             for shoe_config in pricat:
                 writer.writerow(shoe_config)
@@ -59,3 +67,12 @@ class DataProcessing:
     def combine_fields_into_a_new_field(self, source_types, destination_type):
         pass
 
+    def create_structured_dictionary(self, pricat=None):
+        if not pricat:
+            pricat=self.pricat
+
+        for shoe_config in pricat:
+            pass
+
+    def export_as_JSON(self):
+        pass
