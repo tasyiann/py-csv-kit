0eb4c8ce93893a24b2ded50718ba5efc99fda3ed
Anastasios <tasyiann@gmail.com>
2021-10-29 22:56:13 +0300

    updates

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from utils import DataProcessing
 if __name__ == '__main__':
 
     # Initiate Data Processor
-    catalog_processor = DataProcessing("./data/pricat.csv", "./data/mappings.csv")
+    data_processor = DataProcessing("./data/pricat.csv", "./data/mappings.csv")
 
     # Format price catalogue using the mappings.
-    catalog_processor.format_pricat_with_mappings()
+    updated_pricat = data_processor.format_pricat_with_mappings()
 
     # Export updated price catalogue in csv format.
-    catalog_processor.export_pricat_in_csv("./results/mapped_pricat.csv")
+    data_processor.export_in_csv(updated_pricat, "./results/mapped_pricat.csv")
+
+    # Grouping process
+    pass
--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ class DataProcessing:
         self.fieldnames, self.pricat = self._read_csv(pricat_filename)
         _, self.mappings = self._read_csv(mappings_filename)
 
-
     @staticmethod
     def _read_csv(file_name):
         csv_reader = csv.DictReader(open(file_name), delimiter=";")
@@ -28,29 +27,35 @@ class DataProcessing:
             self.fieldnames.append(destination_type)
 
     def format_pricat_with_mappings(self):
+
+        pricat_new = self.pricat.copy()
+
         for m in self.mappings:
 
             # Get all "sub" source_types. (e.g. : size_group_code|size_code)
             source_types = m['source_type'].split('|')
-            for shoe_config in self.pricat:
+            for shoe_config, shoe_config_new in zip(self.pricat, pricat_new):
 
                 # Do the mapping:
                 if self._mapping_detector(m, source_types, shoe_config):
-                    shoe_config[m['destination_type']] = m['destination']
+                    shoe_config_new[m['destination_type']] = m['destination']
 
-                # Remove the old source type if it's mapped to a different type.
+                # Remove the old source type (from pricat_new) if it's mapped to a different type.
                 for source_type in source_types:
-                    if source_type in shoe_config and source_type != m['destination_type']:
-                        shoe_config.pop(source_type)
+                    if source_type in shoe_config_new and source_type != m['destination_type']:
+                        shoe_config_new.pop(source_type)
                         self._update_fieldnames(source_type, m['destination_type'])
 
-    def export_pricat_in_csv(self, filename):
+        return pricat_new
+
+    def export_in_csv(self, pricat, filename):
         # todo: remove empty columns?
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames, delimiter=";")
             writer.writeheader()
-            for shoe_config in self.pricat:
+            for shoe_config in pricat:
                 writer.writerow(shoe_config)
 
     def combine_fields_into_a_new_field(self, source_types, destination_type):
         pass
+
